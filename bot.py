cc0ed8860c720318813360f64bcb8224848ce892
plushkar <75451317+plushkar@users.noreply.github.com>
2020-12-03 16:54:10 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,10 +40,38 @@ async def poll(ctx):
 
     await ctx.message.add_reaction(upvote)
     await ctx.message.add_reaction(downvote)
+    
+    
+    
 
 @client.command()
 async def pizza(ctx):
     await ctx.send("Pizza")
     
+@client.command()
+async def testPoll
+    
+    channel = message.channel
+    choices = {"🇦": "Solos",
+               "🇧": "Duos",
+               "🇨": "Squads"}
+
+    vote = discord.Embed(title="**[POLL]**", description=" ", color=0x00ff00)
+    value = "\n".join("- {} {}".format(*item) for item in choices.items()) 
+    vote.add_field(name="Please vote for the match type:", value=value, inline=True)
+
+    message_1 = await client.send_message(channel, embed=vote)
+
+    for choice in choices:
+        await client.add_reaction(message_1, choice)
+
+    await aynsio.sleep(60)  # wait for one minute
+    message_1 = await client.get_message(channel, message_1.id)
+
+
+    counts = {react.emoji: react.count for react in message_1.reactions}
+    winner = max(choices, key=counts.get)
+
+    await client.send_message(channel, "{} is the winner".format(choices[winner]))
 
 # client.run(TOKEN)
