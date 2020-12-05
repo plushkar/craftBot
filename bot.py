d78a9f6a9b54a9e63f9970ae2e811453f88367d2
plushkar <75451317+plushkar@users.noreply.github.com>
2020-12-04 22:37:57 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,8 +51,8 @@ async def pizza(ctx):
     await ctx.send("Pizza")
 
 @client.command()
-async def mungSpam(ctx, times):
-    for i in range 0 to times:
-        await ctx.send("@everyone munge??)
+async def mungSpam(ctx, times=1):
+    for i in range(min(max(times, 1), 10)):
+        await ctx.send("@everyone munge")
 
 client.run(TOKEN)
